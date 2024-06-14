c8ef9deefe466b5accbc1c8116b6d6b5486b0732
Dylan <mail@dylanlangston.com>
2024-06-14 16:03:24 +0000

    Fix build errors

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
 import { Favorite } from '@mui/icons-material';
 import { useTranslation } from 'react-i18next';
 import Hamburger from './Hamburger';
